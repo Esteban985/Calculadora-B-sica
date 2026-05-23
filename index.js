a8cbdcfb9d505cefeedfdc46bf7e96ff1e7c7722
Esteban Mente <menteesteban5@gmail.com>
2026-05-23 11:08:35 -0600

    correccion del boton de borrar

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ let pantalla = document.querySelector('#pantalla')
 let botones = document.querySelectorAll('.btn')
 let borrar = document.querySelector('.borrar')
 
+
+botones.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+        pantalla.value += btn.textContent
+    })
+})
+
 borrar.addEventListener('click', (event) => {
     if (pantalla.value == "") {
         alert('No hay nada para borrar')
     } else {
         pantalla.value = ""
     }
-})
-
-botones.forEach((btn) => {
-    btn.addEventListener('click', (event) => {
-        pantalla.value += btn.textContent
-    })
 })
